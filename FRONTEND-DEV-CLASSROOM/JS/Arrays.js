79649e8eae5d5c0d1f51f8a9497754a872634cc8
BroPriyansh <priyanshtyagi30@gmail.com>
2025-11-20 13:18:44 +0530

    [Priyansh Tyagi] Class Work Added

---
+++ FRONTEND-DEV-CLASSROOM/JS/Arrays.js
@@ -0,0 +1,37 @@
+// let arr = [1,2,3,4,5];
+
+// let mapArr = arr.map((x) => x*2);
+
+// arr.filter((x) => x%2 == 0);
+
+// arr.reduce((a,b) => a+b);
+// console.log(mapArr);
+// arr.shift();
+
+// arr.unshift(7);
+
+// counters
+
+// console.log("10"+5)
+// console.log(5+"10")
+// console.log(true+5)
+// console.log(null+5)
+// console.log(undefined+5)
+
+// Errors
+
+
+
+// Function Hoisting
+
+function add(){
+    console.log(10+10);
+}
+
+
+d()// TReferenceError: Cannot access 'd' before initialization
+d(); //d = undefined;
+const d = function sub(){
+    console.log("hello");
+}
+
